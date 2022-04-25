f8d83d1c47d737c5fce63f44bb0194dcd212df11
Ankikadu <97692472+Ankikadu@users.noreply.github.com>
2022-04-25 16:45:15 +0530

    files added
    files added

---
+++ book_lab_app/src/components/Books.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Books = () => {
+  return (
+    <div>
+     <h1>Books</h1>   
+     </div>
+  )
+}
---
+++ book_lab_app/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+ export const Navbar = () =>{
+     return(
+        <>
+        <nav className="nav">
+            <h1>Home</h1>
+            <h1>About</h1>
+            <h1>Books</h1>
+            <h1>Login</h1>
+           
+          </nav>
+        </>
+    )
+}
